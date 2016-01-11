dcbd1f3b04ee1c97b214a29216d979d6d916b84b
Tomas <t.monkevicius@gmail.com>
2016-01-11 16:43:13 +0000

    Update SimpleGUI.java
    Slighty updated simplyGUI in order to match our domain. Tomas

--- SimpleGUI.java
+++ SimpleGUI.java
@@ -64,10 +64,10 @@ public class SimpleGUI {
 		Outframe.add(Scroll);
 		Outframe.add(ScrollInfo);		
 
-		JFrame Main = new JFrame("KBS Conversation Bot");   // our main frame
+		JFrame Main = new JFrame("carHire help centre");   // our main frame
 		
 		try {
-    		Main.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("test.jpg")))));
+    		Main.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("test.JPG")))));
     	} catch (IOException e) {
     		e.printStackTrace();
     	}
@@ -93,9 +93,9 @@ public class SimpleGUI {
 		Main.setSize(1200, 800);
 		Main.setVisible(true);                               // Don't forget
 		
-		dialoghistory.add("<H2>Welcome to the Library Helpdesk, please type your question.</H2> " +
-				          "<H3>I can inform you about: The library, its books, its members and " +
-				          "the current lendings. Just ask me.</H3><br>");
+		dialoghistory.add("<H2>Great choice! Now, as you are here you can ask me anything regarding carHire.</H2> " +
+				          "<H3>Including our worldwide vehicles, specific vehicles also information about you " +
+				          "our beloved customer! Don't hesitate to ask me anything! If you need any help, you always can ask for some. You can begin by signing in using username first name and last name command</H3><br>");
 		
 		Output.setText(dialoghistory.firstElement());
 		Input.requestFocusInWindow();
